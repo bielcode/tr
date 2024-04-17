94389d66171cfaf4f139b5b75c7306e07a65a27e
Gabriel Carneiro <biel.dev@outlook.com>
2024-04-17 13:06:48 -0300

    Ajustes nas classes

--- inc/config.class.php
+++ inc/config.class.php
@@ -29,105 +29,113 @@
    @since     2024
    ------------------------------------------------------------------------
  */
-class PluginTrConfig extends CommonDBTM {
-   static protected $notable = true;
-   static function getMenuName() {
-      return __('Tr');
-   }
-   static function getMenuContent() {
-   	global $CFG_GLPI;
-   	$menu = array();
-      $menu['title']   = __('Termo de Responsabilidade','tr');
-      $menu['page']    = "/plugins/tr/front/index.php";
-   	return $menu;
-   }
-   // add tabs
-   function getTabNameForItem(CommonGLPI $item, $withtemplate = 0) {
-      // add ticket tab
-      switch (get_class($item)) {
-         case 'Ticket':
-            return array(1 => __('Termo de Responsabilidade','tr'));
-         default:
-      }
-      // add entity tab
-      switch (get_class($item)) {
-         case 'Entity':
-            return array(1 => __('Dados para T.R.','tr'));
-         default:
-         return '';
-      }
-   }
-   static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0) {
-      switch (get_class($item)) {
-         case 'Ticket':
-            $config = new self();
-            $config->showFormDisplay();
-            break;
-      }
-      switch (get_class($item)) {
-         case 'Entity':
-            $config = new self();
-            $config->showFormDisplayEntity();
-            break;
-      }
-      return true;
-   }
-   // ticket tab
-   function showFormDisplay() {
-      global $CFG_GLPI, $DB;
-      $ID = $_REQUEST['id'];
-      $url = $CFG_GLPI['url_base'];
-      echo "<head>";
-      echo "<script type='text/javascript'>";
-      echo "function setIframeSource() {";
-      echo "var theSelect = document.getElementById('PageType');";
-      echo "var theIframe = document.getElementById('TrIframe');";
-      echo "var theUrl;";
-      echo "theUrl = theSelect.options[theSelect.selectedIndex].value;";
-      echo "theIframe.src = theUrl;";
-      echo "}";
-      echo "</script>";
-      echo "</head>";
-      echo "<body>";
-      echo "<form id='form1' method='post'>";
-      echo "<label>Selecione o Layout </label>";
-      echo "<select id='PageType' onchange='setIframeSource()'>";
-      echo "<option value='$url/plugins/tr/front/tr_pdf.php?id=$ID'>A4</option>";
-      echo "<option value='$url/plugins/tr/front/tr_pdflabel.php?id=$ID'>Label</option>";
-      echo "</select>";
-      echo "</form>";
-      echo "<iframe id='TrIframe' src='$url/plugins/tr/front/tr_pdf.php?id=$ID' frameborder='0' marginwidth='0' marginheight='0' width='80%' height='700'></iframe>";
-      echo "</body>";
-   }
-   // entity tab
-   function showFormDisplayEntity() {
-      global $CFG_GLPI, $DB;
-      // geting rn on db
-      if(isset($_GET['id'])) {
-	      $query_rn = "SELECT * FROM glpi_plugin_os_rn WHERE entities_id = ".$_GET['id'];
-	      $result_rn = $DB->query($query_rn) or die ("erro");
-			$ent_info = $DB->fetchAssoc($result_rn);
-			$EmpresaRn = $ent_info['rn'];
-		}
-		else {
-			$EmpresaRn = '';
-		}
-      
-      $canedit = Session::haveRight(Config::$rightname, UPDATE);
-      if ($canedit) {
-         echo "<form name='form' action='../plugins/os/front/insert_rn.php' method='post'>";
-      }
-      echo "<td>". __('CNPJ: ') ."</td>"; 
-      echo "<td><input type='text' minlength='1' maxlength='18' inputmode='number' name='rn' value=".$EmpresaRn."></td>";
-      echo "</tr>";		           
-      echo "<td colspan='4' class='center'>";
-      echo "</td></tr>";
-      echo "<tr></tr>";
-      echo "<td><input type='hidden' id='id' name='id' value=".$_GET['id']."></td>";           
-      $canedit = Session::haveRight(Config::$rightname, UPDATE);
-      if ($canedit) {         
-         echo "<input type='submit' name='update' class='submit' value=\"" . _sx('button', 'Save') . "\">";
-      }
-      Html::closeForm();
-   }
+class PluginTrConfig extends CommonDBTM
+{
+    protected static $notable = true;
+    static function getMenuName()
+    {
+        return __('Tr');
+    }
+    static function getMenuContent()
+    {
+        global $CFG_GLPI;
+        $menu = [];
+        $menu['title'] = __('Termo de Responsabilidade', 'tr');
+        $menu['page'] = '/plugins/tr/front/index.php';
+        return $menu;
+    }
+    // add tabs
+    function getTabNameForItem(CommonGLPI $item, $withtemplate = 0)
+    {
+        // add ticket tab
+        switch (get_class($item)) {
+            case 'Ticket':
+                return [1 => __('Termo de Responsabilidade', 'tr')];
+            default:
+        }
+        // add entity tab
+        switch (get_class($item)) {
+            case 'Entity':
+                return [1 => __('Dados para T.R.', 'tr')];
+            default:
+                return '';
+        }
+    }
+    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0)
+    {
+        switch (get_class($item)) {
+            case 'Ticket':
+                $config = new self();
+                $config->showFormDisplay();
+                break;
+        }
+        switch (get_class($item)) {
+            case 'Entity':
+                $config = new self();
+                $config->showFormDisplayEntity();
+                break;
+        }
+        return true;
+    }
+    // ticket tab
+    function showFormDisplay()
+    {
+        global $CFG_GLPI, $DB;
+        $ID = $_REQUEST['id'];
+        $url = $CFG_GLPI['url_base'];
+        echo '<head>';
+        echo "<script type='text/javascript'>
+            ";
+            echo 'function setIframeSource() {';
+            echo "var theSelect = document.getElementById('PageType');";
+            echo "var theIframe = document.getElementById('TrIframe');";
+            echo 'var theUrl;';
+            echo 'theUrl = theSelect.options[theSelect.selectedIndex].value;';
+            echo 'theIframe.src = theUrl;';
+            echo '}';
+            echo '
+        </script>';
+        echo '</head>';
+        echo '<body>';
+        echo "<form id='form1' method='post'>";
+        echo '<label>Selecione o Layout </label>';
+        echo "<select id='PageType' onchange='setIframeSource()'>";
+        echo "<option value='$url/plugins/tr/front/tr_pdf.php?id=$ID'>A4</option>";
+        echo "<option value='$url/plugins/tr/front/tr_pdflabel.php?id=$ID'>Label</option>";
+        echo '</select>';
+        echo '</form>';
+        echo "<iframe id='TrIframe' src='$url/plugins/tr/front/tr_pdf.php?id=$ID' frameborder='0' marginwidth='0' marginheight='0' width='80%' height='700'></iframe>";
+        echo '</body>';
+    }
+    // entity tab
+    function showFormDisplayEntity()
+    {
+        global $CFG_GLPI, $DB;
+        // geting rn on db
+        if (isset($_GET['id'])) {
+            $query_rn = 'SELECT * FROM glpi_plugin_tr_rn WHERE entities_id = ' . $_GET['id'];
+            ($result_rn = $DB->query($query_rn)) or die('erro');
+            $ent_info = $DB->fetchAssoc($result_rn);
+            $EmpresaRn = $ent_info['rn'];
+        } else {
+            $EmpresaRn = '';
+        }
+
+        $canedit = Session::haveRight(Config::$rightname, UPDATE);
+        if ($canedit) {
+            echo "<form name='form' action='../plugins/tr/front/insert_rn.php' method='post'>";
+        }
+        echo '<td>' . __('CNPJ: ') . '</td>';
+        echo "<td><input type='text' minlength='1' maxlength='18' inputmode='number' name='rn' value=" . $EmpresaRn . '></td>';
+        echo '</tr>';
+        echo "<td colspan='4' class='center'>";
+        echo '</td></tr>';
+        echo '<tr></tr>';
+        echo "<td><input type='hidden' id='id' name='id' value=" . $_GET['id'] . '></td>';
+        $canedit = Session::haveRight(Config::$rightname, UPDATE);
+        if ($canedit) {
+            echo "<input type='submit' name='update' class='submit' value=\"" . _sx('button', 'Save') . "\">";
+        }
+        Html::closeForm();
+    }
 }
--- inc/profile.class.php
+++ inc/profile.class.php
@@ -29,140 +29,129 @@
    @since     2024
    ------------------------------------------------------------------------
  */
-class PluginOsProfile extends Profile {
-   static $rightname = 'profile';
-   function rawSearchOptions() {
-      $tab = [];
-      $tab[] = ['id'                 => 'common',
-                'name'               => __('OS', 'os')];
-      $tab[] = ['id'                 => '2',
-                'table'              => $this->getTable(),
-                'field'              => 'use',
-                'linkfield'          => 'id',
-                'datatype'           => 'bool'];
-      return $tab;
-   }
-   function showForm($ID, $options=[]) {
-      $profile      = new Profile();
-      if ($canedit = Session::haveRightsOr(self::$rightname, [CREATE, UPDATE, PURGE])) {
-         echo "<form action='".$profile->getFormURL()."' method='post'>";
-      }
-      $profile->getFromDB($ID);
-      $real_right = ProfileRight::getProfileRights($ID, ['plugin_os']);
-      $checked = 0;
-      if (isset($real_right)
-          && ($real_right['plugin_os'] == 1)) {
-         $checked = 1;
-      }
-      echo "<table class='tab_cadre_fixe'>";
-      echo "<tr><th colspan='2' class='center b'>".__('OS', 'os');
-      echo "</th></tr>";
-      echo "<tr class='tab_bg_1'>";
-      echo "<td>".__('OS', 'os')."</td><td>";
-      Html::showCheckbox(['name'    => '_plugin_os',
-                          'checked' => $checked]);
-      echo "</td></tr></table>\n";
-      if ($canedit) {
-         echo "<div class='center'>";
-         echo Html::hidden('id', ['value' => $ID]);
-         echo Html::submit(_sx('button', 'Save'), ['name' => 'update',
-                                                     'class' => 'btn btn-primary',
-                                                    'icon'  => 'ti ti-device-floppy']);
-         echo "</div>\n";
-         Html::closeForm();
-      }
-   }
-   function getTabNameForItem(CommonGLPI $item, $withtemplate=0) {
-      if ($item->getType() == 'Profile') {
-         return __('OS', 'os');
-      }
-      return '';
-   }
-   static function displayTabContentForItem(CommonGLPI $item, $tabnum=1, $withtemplate=0) {
-      if ($item->getType() == 'Profile') {
-         $prof =  new self();
-         $ID = $item->getField('id');
-         $prof->showForm($ID);
-      }
-      return true;
-   }
-   static function addDefaultProfileInfos($profiles_id, $rights, $drop_existing=false) {
-      $profileRight = new ProfileRight();
-      $dbu          = new DbUtils();
-      foreach ($rights as $right => $value) {
-         if ($dbu->countElementsInTable('glpi_profilerights',
-                                        ['profiles_id' => $profiles_id,
-                                         'name'        => $right])
-             && $drop_existing) {
-            $profileRight->deleteByCriteria(['profiles_id' => $profiles_id,
-                                             'name'        => $right]);
-         }
-         if (!$dbu->countElementsInTable('glpi_profilerights',
-                                         ['profiles_id' => $profiles_id,
-                                          'name'        => $right])) {
-               $myright['profiles_id'] = $profiles_id;
-               $myright['name']        = $right;
-               $myright['rights']      = $value;
-               $profileRight->add($myright);
-               $_SESSION['glpiactiveprofile'][$right] = $value;
-         }
-      }
-   }
-   static function createFirstAccess($ID) {
-      self::addDefaultProfileInfos($ID, ['plugin_os' => 1], true);
-   }
-   static function getAllRights($all=false) {
-      $rights = [['itemtype'  => 'PluginOs',
-                  'label'     => __('OS', 'os'),
-                  'field'     => 'plugin_os']];
-      return $rights;
-   }
-   static function initProfile() {
-      global $DB;
-      $profile = new self();
-      $dbu     = new DbUtils();
-      foreach ($profile->getAllRights() as $data) {
-         if ($dbu->countElementsInTable("glpi_profilerights",
-                                        ['name' => $data['field']]) == 0) {
-            ProfileRight::addProfileRights([$data['field']]);
-         }
-      }
-      foreach ($DB->request('glpi_profilerights',
-                            ['profiles_id' => $_SESSION['glpiactiveprofile']['id'],
-                             'name'        => ['LIKE', '%plugin_os%']]) as $prof) {
-         $_SESSION['glpiactiveprofile'][$prof['name']] = $prof['rights'];
-      }
-   }
-   static function removeRightsFromSession() {
-      foreach (self::getAllRights(true) as $right) {
-         if (isset($_SESSION['glpiactiveprofile'][$right['field']])) {
-            unset($_SESSION['glpiactiveprofile'][$right['field']]);
-         }
-      }
-   }
-   static function install(Migration $mig) {
-      global $DB;
-
-      $table = 'glpi_plugin_os_profiles';
-      if (!$DB->tableExists($table)
-          && !$DB->tableExists('glpi_plugin_os_preferences')) {
-         self::initProfile();
-         self::createFirstAccess($_SESSION['glpiactiveprofile']['id']);
-      } else {
-         if ($DB->tableExists($table)
-             && $DB->fieldExists($table,'ID')) {
-            $mig->changeField($table, 'ID', 'id', 'autoincrement');
-         }
-         $profileRight = new ProfileRight();
-         if ($DB->tableExists($table)) {
-            foreach ($DB->request($table, ['use' => 1]) as $data) {
-               $right['profiles_id']   = $data['id'];
-               $right['name']          = "plugin_os";
-               $right['rights']        = $data['use'];
-               $profileRight->add($right);
+class PluginTrProfile extends Profile
+{
+    static $rightname = 'profile';
+    function rawSearchOptions()
+    {
+        $tab = [];
+        $tab[] = ['id' => 'common', 'name' => __('TR', 'tr')];
+        $tab[] = ['id' => '2', 'table' => $this->getTable(), 'field' => 'use', 'linkfield' => 'id', 'datatype' => 'bool'];
+        return $tab;
+    }
+    function showForm($ID, $options = [])
+    {
+        $profile = new Profile();
+        if ($canedit = Session::haveRightsOr(self::$rightname, [CREATE, UPDATE, PURGE])) {
+            echo "<form action='" . $profile->getFormURL() . "' method='post'>";
+        }
+        $profile->getFromDB($ID);
+        $real_right = ProfileRight::getProfileRights($ID, ['plugin_tr']);
+        $checked = 0;
+        if (isset($real_right) && $real_right['plugin_tr'] == 1) {
+            $checked = 1;
+        }
+        echo "<table class='tab_cadre_fixe'>";
+        echo "<tr><th colspan='2' class='center b'>" . __('TR', 'tr');
+        echo '</th></tr>';
+        echo "<tr class='tab_bg_1'>";
+        echo '<td>' . __('TR', 'tr') . '</td><td>';
+        Html::showCheckbox(['name' => '_plugin_tr', 'checked' => $checked]);
+        echo "</td></tr></table>\n";
+        if ($canedit) {
+            echo "<div class='center'>";
+            echo Html::hidden('id', ['value' => $ID]);
+            echo Html::submit(_sx('button', 'Save'), ['name' => 'update', 'class' => 'btn btn-primary', 'icon' => 'ti ti-device-floppy']);
+            echo "</div>\n";
+            Html::closeForm();
+        }
+    }
+    function getTabNameForItem(CommonGLPI $item, $withtemplate = 0)
+    {
+        if ($item->getType() == 'Profile') {
+            return __('TR', 'tr');
+        }
+        return '';
+    }
+    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0)
+    {
+        if ($item->getType() == 'Profile') {
+            $prof = new self();
+            $ID = $item->getField('id');
+            $prof->showForm($ID);
+        }
+        return true;
+    }
+    static function addDefaultProfileInfos($profiles_id, $rights, $drop_existing = false)
+    {
+        $profileRight = new ProfileRight();
+        $dbu = new DbUtils();
+        foreach ($rights as $right => $value) {
+            if ($dbu->countElementsInTable('glpi_profilerights', ['profiles_id' => $profiles_id, 'name' => $right]) && $drop_existing) {
+                $profileRight->deleteByCriteria(['profiles_id' => $profiles_id, 'name' => $right]);
             }
-            $mig->dropTable($table);
-         }
-      }
-   }
+            if (!$dbu->countElementsInTable('glpi_profilerights', ['profiles_id' => $profiles_id, 'name' => $right])) {
+                $myright['profiles_id'] = $profiles_id;
+                $myright['name'] = $right;
+                $myright['rights'] = $value;
+                $profileRight->add($myright);
+                $_SESSION['glpiactiveprofile'][$right] = $value;
+            }
+        }
+    }
+    static function createFirstAccess($ID)
+    {
+        self::addDefaultProfileInfos($ID, ['plugin_tr' => 1], true);
+    }
+    static function getAllRights($all = false)
+    {
+        $rights = [['itemtype' => 'PluginTr', 'label' => __('TR', 'tr'), 'field' => 'plugin_tr']];
+        return $rights;
+    }
+    static function initProfile()
+    {
+        global $DB;
+        $profile = new self();
+        $dbu = new DbUtils();
+        foreach ($profile->getAllRights() as $data) {
+            if ($dbu->countElementsInTable('glpi_profilerights', ['name' => $data['field']]) == 0) {
+                ProfileRight::addProfileRights([$data['field']]);
+            }
+        }
+        foreach ($DB->request('glpi_profilerights', ['profiles_id' => $_SESSION['glpiactiveprofile']['id'], 'name' => ['LIKE', '%plugin_tr%']]) as $prof) {
+            $_SESSION['glpiactiveprofile'][$prof['name']] = $prof['rights'];
+        }
+    }
+    static function removeRightsFromSession()
+    {
+        foreach (self::getAllRights(true) as $right) {
+            if (isset($_SESSION['glpiactiveprofile'][$right['field']])) {
+                unset($_SESSION['glpiactiveprofile'][$right['field']]);
+            }
+        }
+    }
+    static function install(Migration $mig)
+    {
+        global $DB;
+
+        $table = 'glpi_plugin_tr_profiles';
+        if (!$DB->tableExists($table) && !$DB->tableExists('glpi_plugin_tr_preferences')) {
+            self::initProfile();
+            self::createFirstAccess($_SESSION['glpiactiveprofile']['id']);
+        } else {
+            if ($DB->tableExists($table) && $DB->fieldExists($table, 'ID')) {
+                $mig->changeField($table, 'ID', 'id', 'autoincrement');
+            }
+            $profileRight = new ProfileRight();
+            if ($DB->tableExists($table)) {
+                foreach ($DB->request($table, ['use' => 1]) as $data) {
+                    $right['profiles_id'] = $data['id'];
+                    $right['name'] = 'plugin_tr';
+                    $right['rights'] = $data['use'];
+                    $profileRight->add($right);
+                }
+                $mig->dropTable($table);
+            }
+        }
+    }
 }
